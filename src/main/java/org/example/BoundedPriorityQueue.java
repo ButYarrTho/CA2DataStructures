b142cf19cc8f03af7ffb3a27202db74519a61669
AlexC <alexclinton133@gmail.com>
2024-04-28 13:58:50 +0100

    Create BoundedPriorityQueue class extending LinkedList<Appointment> with maxSize and doctorFullName fields

--- src/main/java/org/example/BoundedPriorityQueue.java
+++ src/main/java/org/example/BoundedPriorityQueue.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class BoundedPriorityQueue {
+class BoundedPriorityQueue extends LinkedList<Appointment> {
+    private int maxSize;
+    private String doctorFullName;
+
+    public BoundedPriorityQueue(int maxSize, String doctorFullName) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("Max size must be greater than 0");
+        }
+        this.maxSize = maxSize;
+        this.doctorFullName = doctorFullName.toLowerCase();
+    }
+
 }
